ff664d949e39b6d61c66dfcf9292e526899dcd88
carloseduardov8 <carloseduardov8@hotmail.com>
2017-08-18 16:20:17 -0300

    Make draw lines work

--- Assigment 1/trabalho1.js
+++ Assigment 1/trabalho1.js
@@ -2,21 +2,21 @@ function setup() {
 	createCanvas(1920, 1080);
 }
 
-lines = [];
+var lines = [];
+var curr_line = 0;
 
 function draw() {
 	
-	background(100);
+	background(200);
 	
 	for (i=0; i<lines.length; i++){
-		lines[i].display(mouseX, mouseY);
+		if (i == curr_line){
+			lines[i].display(mouseX, mouseY);
+		} else {
+			lines[i].display_final();
+		}
 	}
 	
-	if (mouseIsPressed) {
-		//line(firstX, firstY, mouseX, mouseY);
-	} else {
-		
-	}
 
 }
 
@@ -24,7 +24,12 @@ function draw() {
 function mousePressed() {
 	nline = new Line(mouseX, mouseY);
 	lines.push(nline);
-			
+}
+
+function mouseReleased() {
+	lines[curr_line].lastX = mouseX;
+	lines[curr_line].lastY = mouseY;
+	curr_line += 1;
 }
 
 //Line class:
@@ -32,8 +37,14 @@ function Line(x, y) {
 	
 	this.firstX = x;
 	this.firstY = y;
+	this.lastX = 0;
+	this.lastY = 0;
 	
 	this.display = function(d_x, d_y){
 		line(this.firstX, this.firstY, d_x, d_y);
+	}
+	
+	this.display_final = function(){
+		line(this.firstX, this.firstY, this.lastX, this.lastY);
 	}
 }
