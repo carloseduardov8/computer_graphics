903cb532d8b828a49f75264e19d822e53d704ac4
carloseduardov8 <carloseduardov8@hotmail.com>
2017-08-27 22:25:46 -0300

    Make drag lines possible

--- Assigment 1/trabalho1.js
+++ Assigment 1/trabalho1.js
@@ -4,8 +4,9 @@ function setup() {
 }
 
 var lines = [];									// Array of lines
-var curr_line = 0;								// Next line to be formed
-var default_dist = 30;							// Distance to decide if a mouse press has hit a line or not
+var ellipses = [];								// Array of intersection ellipses' points
+var default_dist = 30;							// Distance to decide if a mouse press has hit a line or not (for
+												// both edges and middle tests)
 var edit_mode = {"line": -1, "mode": -1};		// Contains line being edited and mode of operation: "create_new",
 												// "edit_first" (to edit first point) or "edit_last"
 
@@ -21,6 +22,9 @@ function draw() {
 				lines[i].display_last(new Point(mouseX, mouseY));
 			} else if (edit_mode["mode"] == "edit_first"){
 				lines[i].display_first(new Point(mouseX, mouseY));
+			} else if (edit_mode["mode"] == "edit_middle"){
+				lines[i].update_points(mouseX, mouseY);
+				lines[i].display_done();
 			}
 		// Case where line has already been formed:
 		} else {
@@ -31,34 +35,47 @@ function draw() {
 				// Intersection between line and itself doesnt make sense:
 				if (j == i) continue;
 				curr_mouse = new Point(mouseX, mouseY);
+				// Now we are going to check for intersections and push them to *ellipses*
+				// so we can draw them later. This allows us to draw them as the last elements 
+				// in the z axis.
 				// Checks intersections for current line being formed:
-				if (j == edit_mode["line"]){
+				if (j == edit_mode["line"] && edit_mode["mode"] != "edit_middle"){
 					if (edit_mode["mode"] == "create_new" || edit_mode["mode"] == "edit_last"){
 						if (j == edit_mode["line"] && doIntersect(lines[i].first, lines[i].last, lines[j].first, curr_mouse)){
 							var intersection = intersectionPoint(lines[i].first, lines[i].last, lines[j].first, curr_mouse);
-							draw_ellipse(intersection);
+							ellipses.push(intersection);
 						}
 					} else if (edit_mode["mode"] == "edit_first"){
 						if (j == edit_mode["line"] && doIntersect(lines[i].first, lines[i].last, curr_mouse, lines[j].last)){
 							var intersection = intersectionPoint(lines[i].first, lines[i].last, curr_mouse, lines[j].last);
-							draw_ellipse(intersection);
+							ellipses.push(intersection);
 						}
 					}
 				// Checks intersections for remaining lines:
 				} else if ( doIntersect(lines[i].first, lines[i].last, lines[j].first, lines[j].last) ) {
 					var intersection = intersectionPoint(lines[i].first, lines[i].last, lines[j].first, lines[j].last);
-					draw_ellipse(intersection);
+					ellipses.push(intersection);
 				}
 			}
 		}
 		
 	}
 	
+	// Draws all the ellipses calculated above:
+	for (i=0; i<ellipses.length; i++){
+		var red = color(160, 11, 26);	
+		fill(red);
+		ellipse(ellipses[i].x, ellipses[i].y, 10, 10);
+	}
+	
+	// Resets the *ellipses* array:
+	ellipses = [];
 
 }
 
 // Actions performed when mouse is pressed (once):
 function mousePressed() {
+	
 	// Checks if the edge of an existing line was selected:
 	for (i=0; i<lines.length; i++){
 		// If first point was selected:
@@ -71,6 +88,16 @@ function mousePressed() {
 			return;
 		}
 	}
+	
+	// Checks if the middle part of an existing line was selected:
+	for (i=0; i<lines.length; i++){
+		if (lines[i].middle_dist(mouseX, mouseY) <= default_dist){
+			lines[i].update_placeholder(mouseX, mouseY);
+			edit_mode = {"line": i, "mode": "edit_middle"};
+			return;
+		}
+	}
+	
 	// Creates a new line and adds it to the array of lines:
 	edit_mode = {"line": lines.length, "mode": "create_new"};
 	lines.push(new Line(new Point(mouseX, mouseY)));
@@ -130,60 +157,50 @@ function doIntersect(p1, q1, p2, q2)
         return true;
  
     // Special Cases
-    // p1, q1 and p2 are colinear and p2 lies on segment p1q1
+    // p1, q1 and p2 are colinear and p2 lies on segment *p1q1*
     if (o1 == 0 && onSegment(p1, p2, q1)) return true;
  
-    // p1, q1 and p2 are colinear and q2 lies on segment p1q1
+    // p1, q1 and p2 are colinear and q2 lies on segment *p1q1*
     if (o2 == 0 && onSegment(p1, q2, q1)) return true;
  
-    // p2, q2 and p1 are colinear and p1 lies on segment p2q2
+    // p2, q2 and p1 are colinear and p1 lies on segment *p2q2*
     if (o3 == 0 && onSegment(p2, p1, q2)) return true;
  
-    // p2, q2 and q1 are colinear and q1 lies on segment p2q2
+    // p2, q2 and q1 are colinear and q1 lies on segment *p2q2*
     if (o4 == 0 && onSegment(p2, q1, q2)) return true;
  
-    return false; // Doesn't fall in any of the above cases
+	// If *p1q1* and *p2q2* dont intersect:
+    return false;
 }
 
 
 // Function that calculates the point of intersection of two lines:
-//DISCLAIMER: This function was inspired by a Flassari.is 2008 post for C++
 function intersectionPoint(p1, q1, p2, q2){
-	// Store the values for fast access and easy
-	// equations-to-code conversion
-	var x1 = p1.x;
-	var x2 = q1.x; 
-	var x3 = p2.x; 
-	var x4 = q2.x;
-	var y1 = p1.y;
-	var y2 = q1.y;  
-	var y3 = p2.y; 
-	var y4 = q2.y;
 	 
-	var d = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4);
-	// If d is zero, there is no intersection:
-	if (d == 0) return undefined;
+	var d = (p1.x - q1.x) * (p2.y - q2.y) - (p1.y - q1.y) * (p2.x - q2.x);
+	
+	// Returns if no intersection was found:
+	if (d == 0) {
+		return undefined;
+	}
 	 
-	// Get the x and y
-	var pre = (x1*y2 - y1*x2);
-	var post = (x3*y4 - y3*x4);
-	var  x = ( pre * (x3 - x4) - (x1 - x2) * post ) / d;
-	var  y = ( pre * (y3 - y4) - (y1 - y2) * post ) / d;
+	// Finds the x and y coordinates:
+	var a = (p1.x*q1.y - p1.y*q1.x);
+	var b = (p2.x*q2.y - p2.y*q2.x);
+	var x = ( a * (p2.x - q2.x) - (p1.x - q1.x) * b ) / d;
+	var y = ( a * (p2.y - q2.y) - (p1.y - q1.y) * b ) / d;
 	 
-	// Check if the x and y coordinates are within both lines
-	if ( x < min(x1, x2) || x > max(x1, x2) ||
-	x < min(x3, x4) || x > max(x3, x4) ) return undefined;
-	if ( y < min(y1, y2) || y > max(y1, y2) ||
-	y < min(y3, y4) || y > max(y3, y4) ) return undefined;
+	// Returns undefined if x and y are in neither lines:
+	if ( x < min(p1.x, q1.x) || x > max(p1.x, q1.x) || x < min(p2.x, q2.x) || x > max(p2.x, q2.x) ){
+		return undefined;
+	} else if ( y < min(p1.y, q1.y) || y > max(p1.y, q1.y) || y < min(p2.y, q2.y) || y > max(p2.y, q2.y) ) {
+		return undefined;
+	}
 	 
-	// Return the point of intersection
+	// Returns an object containing the intersection:
 	return new Point(x, y);
 }
 
-
-function draw_ellipse(point){
-	ellipse(point.x, point.y, 10, 10);
-}
 
 
 // Point class:
@@ -197,6 +214,11 @@ function Line(point) {
 	
 	this.first = point;
 	this.last = undefined;
+	
+	// Placeholder variables to hold given points:
+	this.placeholder = undefined;
+	this.original_first = undefined;
+	this.original_last = undefined;
 	
 	// Draws line from points *first* to *p_mouse*:
 	this.display_last = function(p_mouse){
@@ -213,11 +235,39 @@ function Line(point) {
 		line(this.first.x, this.first.y, this.last.x, this.last.y);
 	}
 	
+	// Updates the *first* and *last* coordinates by adding an offset (mouse distance):
+	this.update_points = function(x, y){
+		this.first.x = this.original_first.x + (x - this.placeholder.x);
+		this.first.y = this.original_first.y + (y - this.placeholder.y);
+		this.last.x = this.original_last.x + (x - this.placeholder.x);
+		this.last.y = this.original_last.y + (y - this.placeholder.y);
+	}
+	
+	//Saves *x* and *y* to *placeholder*:
+	this.update_placeholder = function(x, y){
+		this.placeholder = new Point(x, y);
+		this.original_first = new Point(this.first.x, this.first.y);
+		this.original_last = new Point(this.last.x, this.last.y);
+	}
+	
+	// Returns the distance between *first* and (x,y):
 	this.first_dist = function(x, y){
 		return Math.sqrt( Math.pow(this.first.x-x, 2) + Math.pow(this.first.y-y, 2));
 	}
 	
+	// Returns the distance between *last* and (x,y):
 	this.last_dist = function(x, y){
 		return Math.sqrt( Math.pow(this.last.x-x, 2) + Math.pow(this.last.y-y, 2));
+	}
+	
+	// Returns the distance form points (x,y) to the line:
+	// DISCLAIMER: This function was designed with help from Wikipedia, The Free Encyclopedia
+	this.middle_dist = function(x, y){
+		var numerator = Math.abs( (this.last.y - this.first.y)*x - 
+								  (this.last.x - this.first.x)*y + 
+								   this.last.x*this.first.y - this.last.y*this.first.x );
+		var denominator = Math.sqrt( Math.pow(this.last.y - this.first.y, 2) + 
+									 Math.pow(this.last.x - this.first.x, 2) );
+		return numerator/denominator;
 	}
 }
